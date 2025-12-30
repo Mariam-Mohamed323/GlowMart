0012e3e6ec88142521cc4eb21b26734d555bc465
Mariam-Mohamed323 <mariammohamed3922847@gmail.com>
2025-12-30 23:35:12 +0200

    update get-cart

--- app/api/get-wishlist/route.ts
+++ app/api/get-wishlist/route.ts
@@ -1,24 +1,13 @@
 import { getUserToken } from "@/app/Helpers/getUserToken";
 import { CartResponse } from "@/interfaces";
-import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    // const token = await getUserToken()
-    const cookieStore = cookies();
-  const token = (await cookieStore).get("token")?.value;
-
-  if (!token) {
-    return NextResponse.json(
-      { message: "Unauthorized" },
-      { status: 401 }
-    );
-  }
+    const token = await getUserToken()
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/wishlist`, {
         headers: {
             token: token!
-        },
-      cache: "no-store"
+        }
     });
     const data:CartResponse = await response.json()
     return NextResponse.json(data)
